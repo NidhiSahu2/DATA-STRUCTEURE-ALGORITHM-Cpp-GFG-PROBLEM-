91e84bcabf54d9dc50b57f16d4352ff4c49924af
Nidhi sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2023-12-16 17:31:29 +0530

    Add files via upload

---
+++ array_size.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"input size of array"<<endl;
+	cin>>n;
+	cout<<"enter a value in our"<<endl;
+	int input_array [n];
+	for(int i=0; i<n; i++)
+	cin>>input_array[i];
+	cout<<"output the elements"<<endl;
+	for(int i=0; i<n; i++)
+	cout<<input_array[i]<<" ";
+	return 0;
+
+	return 0;
+	
+}
