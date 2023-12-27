1b6d8b516440ced82195be0d1c03438bc99778d3
Nidhi sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2023-12-27 19:27:27 +0530

    Add files via upload

---
+++ testrecurrsion.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int n)
+{
+
+
+	
+ if(n==0){
+ 
+  return 0;
+}
+
+return n;
+int sum=n/10;
+int val=n%10;
+
+}
+
+int main()
+
+{
+	int n,sum,val;
+	cout<<"Enter a number"<<endl;
+	cin>>n;
+	cout<<"total is"<<add(n)<<sum<<val<<endl;
+	
+}
+
