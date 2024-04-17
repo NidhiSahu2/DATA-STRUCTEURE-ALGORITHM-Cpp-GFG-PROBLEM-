8856e8cf68e7e800dd777c658e18e38cda1a0dfb
Nidhi sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-04-17 16:43:40 +0530

    Add files via upload

---
+++ heapify.cpp
@@ -0,0 +1,86 @@
+
+//how to creeate binary heap //
+#include<iostream>
+using namespace std;
+class heap
+{
+public:
+int arr[100];
+int size=0;
+void insert(int val)
+{
+	size=size+1;
+	int index =size;
+	arr[index]=val;
+	while(index>1)
+	{
+	int parent=index/2;
+     if	(arr[parent]<arr[index])
+	 {
+		swap(arr[parent],arr[index]);
+		index=parent;
+	}
+	else
+	{
+		return;
+	}
+	}
+}
+void print()
+{
+	for(int i=1; i<=size; i++)
+	{
+	cout<<arr[i]<<endl;	
+	}
+	cout<<endl;
+	}			
+};
+
+void heapify(int arr[], int n ,int i)
+{
+int largest=i;
+int left=2*i;
+int right=2*i+1;
+if(left<n && arr[largest]<arr[left])
+{
+	largest=left;
+}
+if(  right<n && arr[largest]<arr[right])
+{
+	largest=right;
+}
+
+if(largest!=1)
+{
+	swap(arr[largest],arr[i]);
+	heapify(arr,n, largest);
+}
+	
+}
+
+
+int main()
+{
+	
+heap h;
+h.insert(57);
+h.insert(55);
+h.insert(53);
+h.insert(54);
+h.insert(52);
+h.insert(51);
+h.print();
+int arr[6]={-1,57,55,53,54,52};
+int n=5;
+for(int i=n/2; i>=0; i--)
+{
+	heapify(arr,n,i);
+}
+cout<<" printing the array now"<<endl;
+for(int  i=1; i<n; i++)
+{
+	cout<<arr[i];
+}cout;
+return 0;
+}
+
