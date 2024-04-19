538cf0af5d44e8b4d9edc58389e72899a34f8170
Nidhi sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-04-19 23:38:09 +0530

    Add files via upload

---
+++ minhesap.cpp
@@ -0,0 +1,25 @@
+//cpp program is show that priority queue i s by //
+//default a mix heap//
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	priority_queue  <int,vector <int>,greater<int> > pq;
+	pq.push(5);
+	pq.push(1);
+	pq.push(10);
+	pq.push(30);
+	pq.push(20);
+	while(pq.empty()==false)
+	{
+		
+		
+		cout<<pq.top()<<" "<<endl;
+		pq.pop();
+		
+	}
+	return 0;
+}
