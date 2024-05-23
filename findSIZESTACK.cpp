c2d48a56c1e7a2b2aba4146996072468cb518a78
Nidhi sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-05-23 23:21:17 +0530

    Add files via upload

---
+++ findSIZESTACK.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+	stack<int>pn;
+	pn.push(2);
+	pn.push(3);
+	pn.push(4);
+	pn.push(25);
+	int size=pn.size();
+	cout<<"size of array"<<size;
+	return 0;
+	
+	
+}
