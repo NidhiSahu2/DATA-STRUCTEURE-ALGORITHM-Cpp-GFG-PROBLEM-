0f963dd8bc6362068c72fc0b9527885bce6b8df7
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-06-04 07:45:14 +0530

    Add files via upload

---
+++ pointrefencebyvalue.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void func1(int num_val)
+{
+	
+}
+void func2(int& num_ref)
+
+{
+	
+}
+int main()
+{
+	int num=5;
+	func1(num);
+	func2(num);
+	return 0;
+};
