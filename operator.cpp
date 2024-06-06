261449b2b87354372d8e2b4b7ed5992371191f27
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-06-06 23:34:34 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=6; int b=9;
+	cout<<"a && b"<<   (a&&b)  <<endl;
+	cout<<"a ! b"<<   (a>b)   <<endl;
+	cout<<"a + b"<<   (a+b)   <<endl;
+	cout<<"!b"<<   (!b)   <<endl;
+	
+}
