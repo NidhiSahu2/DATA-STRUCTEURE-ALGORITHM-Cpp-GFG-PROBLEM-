4f196e6d5161df7afa0b3e081bef2f912300196c
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-06-09 11:34:14 +0530

    Add files via upload

---
+++ deci_to_int,cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double  num1=3.56;
+	cout<<"num_1"<<num1<<endl;
+	
+	int num2 = (int)num1;
+	cout<<"num2 "<< num2<<endl;
+	
+	double num3 = (int)num2;
+	cout<<"num3"<<num3<<endl;
+}
