46b70da8a852c54eaa4459c8f171c687db920103
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-06-27 09:58:48 +0530

    Add files via upload

--- checkpair.cpp
+++ checkpair.cpp
@@ -1,38 +1,33 @@
-// C++ program for the above approach
-#include <bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
-
-// Function to find and print pair
-bool chkPair(int A[], int size, int x)
+bool checkpair (int A[], int size , int x)
 {
-    for (int i = 0; i < (size - 1); i++) {
-        for (int j = (i + 1); j < size; j++) {
-            if (A[i] + A[j] == x) {
-                return 1;
-            }
-        }
-    }
-
-    return 0;
+	for(int i=0; i<size-1; i++)
+	{
+	for(int j= j+i; j<size; j++){
+		
+	
+		if(A[i]+A[j]==x){
+		
+	}
+	
+	}
+};
+	return 1;
 }
 
-// Driver code
 int main()
 {
-    int A[] = { 0, -1, 2, -3, 1 };
-    int x = -2;
-    int size = sizeof(A) / sizeof(A[0]);
-
-    if (chkPair(A, size, x)) {
-        cout << "Yes" << endl;
-    }
-    else {
-        cout << "No" << x << endl;
-    }
-
-    return 0;
-}
-
-// This code is contributed by Samim Hossain Mondal.
-
+	int A[] = {0,-1, 2,3,1};
+	int x=-2;
+	int size =sizeof(A)/sizeof(A[0]);
+	if (checkpair(A,size,x))
+	{
+		cout<<"yes"<<endl;
+	}
+	else
+	{
+		cout<<"NO"<<endl;
+	}
+	return 0;
+} 
