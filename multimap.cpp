1e75bd925ccb800db22d557cdf0fb36efc800add
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-06-28 20:58:48 +0530

    Add files via upload

---
+++ multimap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main()
+{
+	multimap<string,int>map;
+	map.insert make_pair[("urvi" ,3456)];
+	map.insert make_pair[("sneha",6788)]
+	map.insert make_pair[("sneha",1234)]
+	
+	for(auto pair:map)
+	
+	{
+		cout<<"NAME"<<pair.first<<endl;
+		cout<<"PHONE NO"<<pair.second<<endl;
+	}
+	return 0;
+	
+}
