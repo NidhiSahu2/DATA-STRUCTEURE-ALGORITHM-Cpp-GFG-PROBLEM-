38bdca970f65c8eb9f11a9e8ba38858d100793dc
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-07-03 00:34:15 +0530

    Add files via upload

---
+++ nonrepeatingele.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main()
+{
+
+class solution(int arr[], int n)
+{
+	public:
+	unordered_map<int,int>umap;
+	for(int i=0; i<n; i++)
+	
+	  	umap[arr[i]]++;
+	for (int i =0; i<n; i++)
+	
+	{
+		int key = arr[i];
+		temp auto umap.find(key)
+		int key = (temp->second==1);
+		return i+1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+}
