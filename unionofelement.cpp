a688984bb5a01291bb09aec31452a566a5f045df
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-07-03 17:17:02 +0530

    Add files via upload

---
+++ unionofelement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	set<int>ans;
+	for(int i=0; i<n; i++)
+	{
+		ans.insert a[i];
+	}
+	
+	for(int i=0; i<n; i++)
+	{
+		ans.insert b[i];
+	}
+	
+	return ans.size();
+};
